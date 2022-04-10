d9a85e63f1db52d5cc27c7778b6f99b871485917
shivani rana <63535949+shivani6320@users.noreply.github.com>
2022-04-11 00:59:31 +0530

    R Variables & Operators

---
+++ Day 03 : R Variables & Operators/Operators & Variables.R
@@ -0,0 +1,17 @@
+# Initialization of variables
+
+# using equal to operator
+var1 = "hello"
+print(var1)
+
+# using leftward operator
+var2 <- "world"
+print(var2)
+
+# using rightward operator
+"sun" -> var3
+print(var3)
+print(class(var1))
+ls() 
+rm(var3)
+print(var3)
